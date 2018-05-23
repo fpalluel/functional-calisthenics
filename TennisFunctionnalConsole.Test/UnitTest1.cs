dbd4db42888d4dc4ad1b7ba45705d6e16f695532
Mathieu Cans <mathieucans@gmail.com>
2018-05-23 20:17:23 +0200

    tentative curry

---
+++ TennisFunctionnalConsole.Test/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisFunctionnalConsole.Test
+{
+    [TestClass]
+    public class TennisScoreShould
+    {
+        [TestMethod]
+        public void display_love_love_at_startup()
+        {
+            var input = new[] {"Exit"};
+            var outputs = new[] {"love-love", "bye bye\r\n"};
+
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            Console.SetIn(new StringReader(string.Join("\r\n", input)));
+
+            var line = "";
+            Program.Main(new string[] {});
+
+            Assert.AreEqual(string.Join("\r\n", outputs), stringWriter.GetStringBuilder().ToString());
+        }
+
+        [TestMethod]
+        public void display_fifteen_love_if_player_one_wins()
+        {
+            var input = new[] { "P1", "Exit" };
+            var outputs = new[] { "love-love", "fifteen-love", "bye bye\r\n" };
+
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            Console.SetIn(new StringReader(string.Join("\r\n", input)));
+
+            var line = "";
+            Program.Main(new string[] { });
+
+            Assert.AreEqual(string.Join("\r\n", outputs), stringWriter.GetStringBuilder().ToString());
+        }
+    }
+}
+
---
+++ TennisFunctionnalConsole/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace TennisFunctionnalConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var initial = new Score("love-love");
+            Console.WriteLine(initial.Display);
+          
+            CalcScore(Curry(Console.ReadLine())(initial));
+        }
+
+        private static Func<Score, InputData> Curry(string readLine)
+        {
+            return (s) => new InputData(readLine, s);
+        }
+
+        private static void CalcScore(InputData data)
+        {           
+            if (data.ReadLine != "Exit")
+            {
+                var score = data.Score.P1Win();
+                Console.WriteLine(score.Display);
+                CalcScore(new InputData(Console.ReadLine(), score));
+            }
+            else
+            {
+                Console.WriteLine("bye bye");
+            }
+        }
+
+    }
+
+    public class InputData
+    {
+        public string ReadLine { get; set; }
+        public Score Score { get; set; }
+
+        public InputData(string readLine, Score score)
+        {
+            ReadLine = readLine;
+            Score = score;
+        }
+    }
+
+    public class Score
+    {
+        public Score(string display)
+        {
+            Display = display;
+        }
+
+        public string Display { get; private set; }
+
+        public Score P1Win()
+        {
+            return new Score("fifteen-love");
+        }
+    }
+}
