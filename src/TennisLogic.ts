e022ab58959c453a9b701caf6dd2e301c3d366e2
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-05-29 00:15:44 +0200

    implement some of the logic

--- src/TennisLogic.ts
+++ src/TennisLogic.ts
@@ -1,13 +1,6 @@
 export type ScoreAndBallWinner = { score: Score, ballWinner: string };
-type Score = [number, number]
+export type Score = [number, number]
 type ScoreWithFormatting = { score: Score, formattedScore: string }
-
-function toScoreWithFormatting(nextScoreValue) {
-    return {
-        score: nextScoreValue,
-        formattedScore: formatScore(nextScoreValue)
-    };
-}
 
 export function nextScoreWithFormatting({score, ballWinner}: ScoreAndBallWinner): ScoreWithFormatting {
     let nextScoreValue = nextScore(score)(ballWinner); // violates no naming of values only functions
@@ -19,9 +12,29 @@ export function createInitialScore(): ScoreWithFormatting {
     return {score: initialScore, formattedScore: formatScore(initialScore)};
 }
 
-function formatScore(score: Score) {
-    // Fake todo implement
-    return score.toString()
+function toScoreWithFormatting(score) {
+    return {
+        score: score,
+        formattedScore: formatScore(score)
+    };
+}
+
+function getPointScore(points: number) {
+    return {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}[points];
+}
+
+export function formatScore([p1Points, p2Points]: Score) {
+
+    if (p1Points === p2Points) {
+        if (p1Points >= 3) {
+            return 'Deuce';
+        } else {
+            return getPointScore(p1Points) + "-" + "All"
+        }
+    } else {
+        return getPointScore(p1Points)  + "-" + getPointScore(p2Points)
+
+    }
 }
 
 function nextScore(score: Score): (ballWinner: any) => Score {
---
+++ tests/TennisLogic.test.ts
@@ -0,0 +1,73 @@
+import {equal} from "assert";
+import {formatScore, nextScoreWithFormatting, Score} from "../src/TennisLogic";
+import { assert, expect } from 'chai';
+
+describe('TennisLogic', () => {
+
+    describe('nextScoreWithFormatting', () => {
+        const equalScores = [
+            [0, 0, "Love-All"],
+            [1, 1, "Fifteen-All"],
+            [2, 2, "Thirty-All"],
+            [3, 3, "Deuce"],
+            [4, 4, "Deuce"],
+
+        ]
+        const pointScores = [
+            [1, 0, "Fifteen-Love"],
+            [0, 1, "Love-Fifteen"],
+            [2, 0, "Thirty-Love"],
+            [0, 2, "Love-Thirty"],
+            [3, 0, "Forty-Love"],
+            [0, 3, "Love-Forty"],
+
+            [2, 1, "Thirty-Fifteen"],
+            [1, 2, "Fifteen-Thirty"],
+            [3, 1, "Forty-Fifteen"],
+            [1, 3, "Fifteen-Forty"],
+
+            [3, 2, "Forty-Thirty"],
+            [2, 3, "Thirty-Forty"],
+        ]
+        const endOfGame = [
+            [4, 0, "Win for player1"],
+            [0, 4, "Win for player2"],
+
+            [4, 1, "Win for player1"],
+            [1, 4, "Win for player2"],
+
+            [4, 2, "Win for player1"],
+            [2, 4, "Win for player2"],
+
+            [4, 3, "Advantage player1"],
+            [3, 4, "Advantage player2"],
+            [5, 4, "Advantage player1"],
+            [4, 5, "Advantage player2"],
+            [15, 14, "Advantage player1"],
+            [14, 15, "Advantage player2"],
+
+            [6, 4, "Win for player1"],
+            [4, 6, "Win for player2"],
+            [16, 14, "Win for player1"],
+            [14, 16, "Win for player2"]
+        ]
+
+        equalScores.forEach(([p1, p2, result]) =>{
+            it('equal scores', () => {
+                let score: Score = [p1, p2] as Score;
+                expect(formatScore(score)).equal(result)
+            });
+            
+            it('point scores', () => {
+                
+            });
+        })
+
+        pointScores.forEach(([p1, p2, result]) =>{
+            it('point scores', () => {
+                let score: Score = [p1, p2] as Score;
+                expect(formatScore(score)).equal(result)
+            });
+        });
+    });
+});
